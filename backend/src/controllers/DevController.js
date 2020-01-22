bdf6ee273b81960efc8a891d76698707a1da5076
Cassiano Maia <cassmala@gmail.com>
2020-01-22 09:14:50 -0300

    Colocando status codes nas respostas da api

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -14,7 +14,8 @@ module.exports = {
     async show(request, response) {
         const { github_username } = request.params;
         let dev = await Dev.findOne({ github_username });
-        return response.json(dev);
+        const statusCode = dev ? 200 : 404;
+        return response.status(statusCode).json(dev);
     },
 
     async store (request, response) {
@@ -22,29 +23,31 @@ module.exports = {
 
         let dev = await Dev.findOne({ github_username });
 
-        if(!dev){
-            const apiResponse =  await axios.get(`https://api.github.com/users/${github_username}`);
-            
-            const { name = login, avatar_url, bio } = apiResponse.data;
-
-            const techsArray = parsetStringAsArray(techs);
-            
-            const location = {
-                type: 'Point',
-                coordinates: [longitude, latitude],
-            }
-
-            dev = await Dev.create({
-                github_username,
-                name,
-                avatar_url,
-                bio,
-                techs: techsArray,
-                location,
-            });
+        if(dev){
+            return response.status(200).json(dev);
         }
         
-        return response.json(dev);
+        const apiResponse =  await axios.get(`https://api.github.com/users/${github_username}`);
+            
+        const { name = login, avatar_url, bio } = apiResponse.data;
+
+        const techsArray = parsetStringAsArray(techs);
+        
+        const location = {
+            type: 'Point',
+            coordinates: [longitude, latitude],
+        }
+
+        dev = await Dev.create({
+            github_username,
+            name,
+            avatar_url,
+            bio,
+            techs: techsArray,
+            location,
+        });
+
+        return response.status(201).json(dev);
     },
 
     async update (request, response){
@@ -75,7 +78,8 @@ module.exports = {
 
     async destroy(request, response){
         const { github_username } = request.params;
-        let dev = await Dev.findOneAndDelete({ github_username });
-        return response.json(dev);
+        const dev = await Dev.findOneAndDelete({ github_username });
+        const statusCode = dev ? 204 : 404;
+        return response.status(statusCode).json(dev);
     }
 };
